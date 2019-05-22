d8312b0f81f52104de07283baa962914e2a4dd38
tnkTaka <taka.abc.hiko@gmail.com>
2019-05-23 08:04:49 +0900

    add css

--- index.php
+++ index.php
@@ -34,13 +34,14 @@ function DrawFortune($fortune){
 <html lang = "ja">
 <head>
     <meta charset = "UTF-8">
+    <link rel="stylesheet" type="text/css" href="./assets/css/style.css">
     <title>WP41</title>
 </head>
     <body>
-        <center>
-        <h1><?php echo $_fortune[$_res]["luck"]?></h1>
-        <p><?php echo $_fortune[$_res]["message"]?></p>
-        <img src=<?php echo $_fortune[$_res]["image"]?> alt="おみくじ結果">
-        </center>
+        <div class="center">
+            <h1><?php echo $_fortune[$_res]["luck"]?></h1>
+            <p><?php echo $_fortune[$_res]["message"]?></p>
+            <img src=<?php echo $_fortune[$_res]["image"]?> alt="おみくじ結果">
+        </div>
     </body>
 </html>
